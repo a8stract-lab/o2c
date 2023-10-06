8b11a6b70325de7ad473194490a9e3e778e0a755
Zicheng Wang <wzc@smail.nju.edu.cn>
2023-10-06 09:17:17 -0600

    sync

--- evaluation-projects/ab-run.sh
+++ evaluation-projects/ab-run.sh
@@ -11,10 +11,10 @@ mkdir -p ab-res
 
 # Loop to execute the commands 20 times
 for i in {1..20}; do
-  ab -n 1000 -c 10 "http://[::1]:8000/100kb.test" >> "ab-res/1005-$1-100kb-c10.txt"
-  ab -n 1000 -c 100 "http://[::1]:8000/100kb.test" >> "ab-res/1005-$1-100kb-c100.txt"
-  ab -n 1000 -c 10 "http://[::1]:8000/1mb.test" >> "ab-res/1005-$1-1mb-c10.txt"
-  ab -n 1000 -c 100 "http://[::1]:8000/1mb.test" >> "ab-res/1005-$1-1mb-c100.txt"
-  ab -n 1000 -c 10 "http://[::1]:8000/10mb.test" >> "ab-res/1005-$1-10mb-c10.txt"
-  ab -n 1000 -c 100 "http://[::1]:8000/10mb.test" >> "ab-res/1005-$1-10mb-c100.txt"
+  # ab -n 1000 -c 10 "http://[::1]:8000/100kb.test" >> "ab-res/$1-100kb-c10.txt"
+  ab -n 1000 -c 32 "http://[::1]:8000/100kb.test" >> "ab-res/$1-100kb-c32.txt"
+  # ab -n 1000 -c 10 "http://[::1]:8000/1mb.test" >> "ab-res/$1-1mb-c10.txt"
+  ab -n 1000 -c 32 "http://[::1]:8000/1mb.test" >> "ab-res/$1-1mb-c32.txt"
+  # ab -n 1000 -c 10 "http://[::1]:8000/10mb.test" >> "ab-res/$1-10mb-c10.txt"
+  ab -n 1000 -c 32 "http://[::1]:8000/10mb.test" >> "ab-res/$1-10mb-c32.txt"
 done
